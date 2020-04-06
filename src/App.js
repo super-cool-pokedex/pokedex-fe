85dba4883014a72a30c624570c776b26b9e6fc4f
Robert Driskell <driskell.ba@gmail.com>
2020-04-06 18:37:01 -0500

    pokemonList component created.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,12 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 import "./App.css";
+import PokemonList from "./components/pokemonList";
 
 function App() {
-    const [pokeArr, setPokeArr] = useState([]);
-
-    const [prev, setPrev] = useState(null);
-    const [next, setNext] = useState(null);
-
-    useEffect(() => {
-        axios
-            .get(`https://pokeapi.co/api/v2/pokemon`)
-            .then((res) => {
-                setPokeArr(res.data.results);
-                setNext(res.data.next);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }, []);
-
-    const loadNext = () => {
-        axios
-            .get(`${next}`)
-            .then((res) => {
-                setPokeArr(res.data.results);
-                setNext(res.data.next);
-                setPrev(res.data.previous);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-
-    const loadPrevious = () => {
-        axios
-            .get(`${prev}`)
-            .then((res) => {
-                setPokeArr(res.data.results);
-                setPrev(res.data.previous);
-                setNext(res.data.next);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
-
     return (
         <div className="App">
-            {pokeArr.map((pokemon) => (
-                <div>{pokemon.name}</div>
-            ))}
-            {
-                prev ? 
-                <button onClick={loadPrevious}>Previous</button>
-                :
-                <></>
-            }
-            {
-                next ?
-                <button onClick={loadNext}>Next</button>
-                :
-                <></>
-            }
+            <PokemonList />
         </div>
     );
 }
